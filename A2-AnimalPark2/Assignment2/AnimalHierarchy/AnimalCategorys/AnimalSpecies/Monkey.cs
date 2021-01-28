cd7567786b97ac4236970c8c4b79ca49ff9a8486
Filip Moreau <filip.moreau@hotmail.com>
2021-01-28 12:55:53 +0100

    Created new Solution (A2) from A1-copy

---
+++ A2-AnimalPark2/Assignment2/AnimalHierarchy/AnimalCategorys/AnimalSpecies/Monkey.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Assignment1
+{
+    class Monkey : Mammal
+    {
+        public float TailLength { get; private set; }   //measured in centimeters (cm)
+
+
+        /// <summary>
+        /// Constructor called when creating a monkey
+        /// </summary>
+        public Monkey(string name, int age, Genders gender, Image image, int numberOfTeeth, Color furColor, float tailLength) : base(name, age, gender, image, numberOfTeeth, furColor)
+        {
+            this.TailLength = tailLength;
+        }
+
+
+        /// <summary>
+        /// Prints a list of all monkey-attributes
+        /// </summary>
+        public override List<string> ToString()
+        {
+            List<string> text = base.ToString();
+            text.Add($"Species:  Monkey");
+            text.Add($"Tail length:  {TailLength} cm");
+
+            return text;
+        }
+    }
+
+}
---
+++ A2-AnimalPark2/Assignment2/AnimalHierarchy/AnimalCategorys/Mammal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Assignment1
+{
+    abstract class Mammal : Animal
+    {
+        public int NumberOfTeeth { get; private set; }
+        public Color FurColor { get; private set; }
+
+
+        /// <summary>
+        /// Constructor called when creating a mammal
+        /// </summary>
+        protected Mammal(string name, int age, Genders gender, Image image, int numberOfTeeth, Color furColor) : base(name, age, gender, image)
+        {
+            this.NumberOfTeeth = numberOfTeeth;
+            this.FurColor = furColor;
+        }
+
+
+        /// <summary>
+        /// Prints a list of all mammal-attributes
+        /// </summary>
+        public override List<string> ToString()
+        {
+            List<string> text = base.ToString();
+            text.Add($"Category:  Mammal");
+            text.Add($"Number of teeth:  {NumberOfTeeth}");
+            text.Add($"Fur color:  ({FurColor.R},{FurColor.G},{FurColor.B})");
+
+            return text;
+        }
+    }
+
+}
