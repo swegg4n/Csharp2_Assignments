b3b257de27bfa8dfca2aa5b14490c1ff98bcdce6
Filip Moreau <filip.moreau@hotmail.com>
2021-04-06 22:06:37 +0200

    Form created. Invoice class representation created. Loading from file Done. Calculate totals Done

---
+++ A6-InvoiceReader/Assignment6/Invoice.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Assignment6
+{
+    class Invoice
+    {
+        public Invoice(string[] lines)
+        {
+            int ptr = 0;
+
+            InvoiceNr = int.Parse(lines[ptr++]);
+            InvoiceDate = lines[ptr++];
+            DueDate = lines[ptr++];
+            RecieverContact = new Contact(lines, ref ptr);
+
+            int itemsCount = int.Parse(lines[ptr++]);
+            Items = new Item[itemsCount];
+            for (int i = 0; i < itemsCount; i++)
+            {
+                Items[i] = new Item(lines, ref ptr);
+            }
+
+            SenderContact = new Contact(lines, ref ptr);
+            PhoneNr = lines[ptr++];
+            WebPageURL = lines[ptr++];
+        }
+
+        public int InvoiceNr { get; private set; }
+        public string InvoiceDate { get; private set; }
+        public string DueDate { get; private set; }
+
+        public Contact RecieverContact { get; private set; }
+        public Item[] Items { get; private set; }
+        public Contact SenderContact { get; private set; }
+
+        public string PhoneNr { get; private set; }
+        public string WebPageURL { get; private set; }
+
+
+        public new string ToString()
+        {
+            string returnString =
+                $"{InvoiceNr}\n" +
+                $"{InvoiceDate}\n" +
+                $"{DueDate}\n" +
+                $"{RecieverContact}\n";
+
+            for (int i = 0; i < Items.Length; i++)
+                returnString += Items[i].ToString() + "\n";
+
+            returnString +=
+                $"{SenderContact}\n" +
+                $"{PhoneNr}\n" +
+                $"{WebPageURL}";
+
+            return returnString;
+        }
+    }
+
+    class Contact
+    {
+        public Contact(string[] lines, ref int ptr)
+        {
+            CompanyName = lines[ptr++];
+            PersonName = lines[ptr++];
+            Address = lines[ptr++];
+            Zipcode = lines[ptr++];
+            City = lines[ptr++];
+            Country = lines[ptr++];
+        }
+
+        public string CompanyName { get; private set; }
+        public string PersonName { get; private set; }
+        public string Address { get; private set; }
+        public string Zipcode { get; private set; }
+        public string City { get; private set; }
+        public string Country { get; private set; }
+
+
+        public new string ToString()
+        {
+            return $"{CompanyName}\n" +
+                   $"{PersonName}\n" +
+                   $"{Address}\n" +
+                   $"{Zipcode}\n" +
+                   $"{City}\n" +
+                   $"{Country}";
+        }
+    }
+
+    class Item
+    {
+        public Item(string[] lines, ref int ptr)
+        {
+            Description = lines[ptr++];
+            Quantity = int.Parse(lines[ptr++]);
+            Price = float.Parse(lines[ptr++]);
+            TaxPercentage = float.Parse(lines[ptr++]);
+        }
+
+        public string Description { get; private set; }
+        public int Quantity { get; private set; }
+        public float Price { get; private set; }
+        public float TaxPercentage { get; private set; }
+        public float TotalTax { get { return Price * (TaxPercentage / 100) * Quantity; } }
+        public float Total { get { return Price * Quantity + TotalTax; } }
+
+
+        public new string ToString()
+        {
+            return $"{Description}\n" +
+                   $"{Quantity}\n" +
+                   $"{Price}\n" +
+                   $"{TaxPercentage}\n" +
+                   $"{Total}";
+        }
+    }
+
+}
---
+++ A6-InvoiceReader/Assignment6/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadFile("F:/Programming/Csharp2_Assignments/A6-InvoiceReader/Assignment6/Invoices/Invoice_1.txt");
+        }
+
+        Invoice loadedInvoice;
+
+
+        private void DiscountPercentage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (loadedInvoice != null)
+                CalculateDiscountTotal();
+        }
+
+
+        private void LoadFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            loadedInvoice = new Invoice(lines);
+
+            CompanyName.Text = loadedInvoice.SenderContact.CompanyName;
+            InvoiceNr.Text = loadedInvoice.InvoiceNr.ToString();
+            InvoiceDate.Text = loadedInvoice.InvoiceDate;
+            DueDate.Text = loadedInvoice.DueDate;
+
+            RecieverBlock.Text = loadedInvoice.RecieverContact.CompanyName + "\n" +
+                                 loadedInvoice.RecieverContact.PersonName + "\n" +
+                                 loadedInvoice.RecieverContact.Address + "\n" +
+                                 loadedInvoice.RecieverContact.Zipcode + "  " + loadedInvoice.RecieverContact.City + "\n" +
+                                 loadedInvoice.RecieverContact.Country;
+
+            for (int i = 0; i < loadedInvoice.Items.Length; i++)
+            {
+                ItemsList.Items.Add(loadedInvoice.Items[i]);
+            }
+
+            AddressBlock.Text = loadedInvoice.SenderContact.Address + "\n" +
+                                loadedInvoice.SenderContact.Zipcode + "  " + loadedInvoice.RecieverContact.City + "\n" +
+                                loadedInvoice.SenderContact.Country;
+
+            ContactBlock.Text = loadedInvoice.PhoneNr + "\n" +
+                                loadedInvoice.WebPageURL;
+
+            TotalSum.Text = loadedInvoice.Items.Sum(x => x.Total).ToString();
+            CalculateDiscountTotal();
+        }
+
+
+        private void CalculateDiscountTotal()
+        {
+            float discountPercentage = 0;
+            float.TryParse(DiscountPercentage.Text, out discountPercentage);
+
+            discountPercentage = (discountPercentage < 0.0f) ? 0.0f : discountPercentage;
+            discountPercentage = (discountPercentage > 100.0f) ? 100.0f : discountPercentage;
+
+            float total = loadedInvoice.Items.Sum(x => x.Total);
+            DiscountedTotalSum.Text = (total * (1 - discountPercentage / 100)).ToString();
+        }
+    }
+
+}
